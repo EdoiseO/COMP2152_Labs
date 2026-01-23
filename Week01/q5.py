6110f9df6889b03408ff9308f595a26735ccc6a4
Phillip Onofua <edoisephillip2@gmail.com>
2026-01-23 10:36:16 -0500

    Complete Week 1

--- Week01/q5.py
+++ Week01/q5.py
@@ -2,4 +2,4 @@ userAge = int(input("Enter your age: "))
 userAge = userAge + 22
 print("Now showing the shop items filtred by age ", userAge)
 
-# git add . && git commit -m "add lab 01" && git push 
+# git add . && git commit -m "add lab 01" && git push s
